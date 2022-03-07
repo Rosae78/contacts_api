b9940de8f8731133975cb60bf9613201ae835d8f
Rosa <FormacionRosaMariaAbraham@gmail.com>
2022-03-07 12:12:01 +0100

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 require('dotenv').config();
 
 //Conectamos a la DB
